468f35b89119c817da334c18861c798faf9b005f
xypecho <xypecho@163.com>
2017-12-22 23:06:39 +0800

    upda

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,6 @@
 <template>
 	<transition name="move">
-	<div class="food" v-show="showflag">
+	<div class="food" v-show="showflag" ref="food">
 		<div class="food-content">
 			<div class="image-title">
 				<img :src="food.image">
@@ -23,16 +23,16 @@
 				</div>
 				<div @click="selectfirst(food)" class="buy" v-show="!food.count || food.count==0">加入购物车</div>
 			</div>
-		</div>
-		<split></split>
-		<div class="foosinfo">
-			<h1 class="title">商品介绍</h1>
-			<p class="info">{{food.info}}</p>
-		</div>
-		<split></split>
-		<div class="ratings">
-			<h1>商品评价</h1>
-			<ratingselect></ratingselect>	
+			<split v-show="food.info"></split>
+			<div class="foosinfo" v-show="food.info">
+				<h1 class="title">商品介绍</h1>
+				<p class="info">{{food.info}}</p>
+			</div>
+			<split></split>
+			<div class="ratings">
+				<h1>商品评价</h1>
+				<ratingselect :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>	
+			</div>
 		</div>
 	</div>
 	</transition>
@@ -44,6 +44,9 @@
 	import ratingselect from '../../components/ratingselect/ratingselect'
 	import Vue from 'vue'
 	import BScroll from 'better-scroll'
+	// const POSITIVE=0;
+	// const NEGATIVE=1;
+	const ALL=2;
 	export default{
 		props:{
 			food:{
@@ -52,12 +55,30 @@
 		},
 		data(){
 			return{
-				showflag:false
+				showflag:false,
+				selectType:ALL,
+				onlyContent:true,
+				desc:{
+						all:"全部",
+						positive:"推荐",
+						negative:"吐槽"
+				}
 			}
 		},
 		methods:{
 			show(){
 				this.showflag=true;
+				this.selectType=ALL;
+				this.onlyContent=true;
+				this.$nextTick(() => {
+					if (!this.scroll) {
+					  this.scroll = new BScroll(this.$refs.food,{
+					    click: true
+					  });
+					} else {
+					  this.scroll.refresh();
+					}
+				})
 			},
 			hide(){
 				this.showflag=false;
@@ -163,4 +184,12 @@
 				font-weight:200
 				color:rgb(77,85,93)
 				line-height:24px
+		.ratings
+			padding-top:18px
+			h1
+				line-height:14px
+				margin-left:18px
+				font-size:14px
+				color:rgb(7,17,27)
+				
 </style>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,12 +1,20 @@
 <template>
 	<div class="ratingselect">
-		
+		<div class="rating-type border-1px">
+		  <span class="block positive" :class="{'active':selectType==2}">{{desc.all}}<span class="count">12</span></span>
+		  <span class="block positive" :class="{'active':selectType==1}">{{desc.positive}}<span class="count">123</span></span>
+		  <span class="block negative" :class="{'active':selectType==0}">{{desc.negative}}<span class="count">311</span></span>
+		</div>
+		<div class="switch">
+		  <span class="icon-check_circle"></span>
+		  <span class="text">只看有内容的评价</span>
+		</div>
 	</div>
 </template>
 
 <script>
-	const POSITIVE=0;
-	const NEGATIVE=1;
+	// const POSITIVE=0;
+	// const NEGATIVE=1;
 	const ALL=2;
 	export default{
 		props:{
@@ -39,5 +47,35 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-	
+	@import '../../common/stylus/mixin.styl';
+	.ratingselect
+		.rating-type
+			padding:18px 0
+			margin:0 18px
+			border-1px(rgba(7,17,27,0.1))
+			font-size:0
+			.block
+				display:inline-block
+				padding:8px 12px 8px 12px
+				text-align:center
+				line-height:16px
+				height:16px
+				margin-right:8px
+				font-size:12px
+				border-radius:2px
+				color: rgb(77, 85, 93)
+				&.active
+					color:#fff
+				.count
+					font-size:8px
+					margin-left:2px
+				&.positive
+					background-color:rgba(0,160,220,0.2)
+					&.active
+						background-color:rgb(0,160,220)
+				&.negative
+					background-color:rgba(77,85,93,0.2)
+					&.active
+						background-color:rgb(77,85,93)
+						
 </style>
